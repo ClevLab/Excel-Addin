152afc011dc72fe30869369bc50d38f4cfecaad6
Aurélien Lemoine <lemoinea@clevlab.fr>
2018-04-30 18:49:55 +0200

    Commit des sources du projet demo

---
+++ ExcelAddIn/ExcelServices.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelAddIn
+{
+    public static class ExcelServices
+    {
+        #region [ Methods ]
+        public static Workbook GetCurrentExcel()
+        {
+            Application currentApp = Globals.ThisAddIn.Application;
+            return currentApp.ActiveWorkbook;
+        }
+
+        public static List<String> GetAllWorksheetsName(Workbook document)
+        {
+            List<String> listWorksheetsName = null;
+
+            if (document?.Worksheets?.Count > 0)
+            {
+                listWorksheetsName = new List<string>();
+
+                foreach (Worksheet worksheet in document.Worksheets)
+                {
+                    if (!String.IsNullOrWhiteSpace(worksheet.Name))
+                        listWorksheetsName.Add(worksheet.Name);
+                }
+            }
+
+            return listWorksheetsName;
+        }
+
+        /// <summary>
+        /// Get the selected worksheet
+        /// </summary>
+        /// <param name="document">Excel document</param>
+        /// <param name="worksheetName">name of worksheet to find</param>
+        /// <returns>The selected worksheet or null</returns>
+        public static Worksheet GetSelectedWorksheet(Workbook document, String worksheetName)
+        {
+            Worksheet selectedWorksheet = null;
+
+            if (document?.Worksheets?.Count > 0 && !String.IsNullOrWhiteSpace(worksheetName))
+                selectedWorksheet = document.Worksheets.Cast<Worksheet>().FirstOrDefault(w => w != null && w.Name == worksheetName);
+
+            return selectedWorksheet;
+        }
+
+        public static void DeleteWorksheetsWithName(Workbook document, String nameToDelete)
+        {
+            if (document?.Worksheets?.Count > 0 && !String.IsNullOrWhiteSpace(nameToDelete))
+            {
+                //The index of Worksheets starts to 1 (and not 0)
+                for (int i = document.Worksheets.Count; i > 0; i--)
+                {
+                    //Compare the names and ignore the caps
+                    if (document.Worksheets[i] is Worksheet worksheet && worksheet.Name?.ToLowerInvariant() == nameToDelete.ToLowerInvariant())
+                        worksheet.Delete();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Search the column where is the text
+        /// </summary>
+        /// <param name="worksheet">Worksheet where realize the searching</param>
+        /// <param name="rowIndex">Index of the row where search the text</param>
+        /// <param name="textToSearch">Text to search</param>
+        /// <returns></returns>
+        public static int GetColumnIndex(Worksheet worksheet, int rowIndex, String textToSearch)
+        {
+            int columnFound = -1;
+
+            if (worksheet != null && rowIndex > 0 && !String.IsNullOrWhiteSpace(textToSearch))
+            {
+                int currentColumnIndex = 1;
+                while (columnFound == -1 && currentColumnIndex < 100)
+                {
+                    Range currentCell = worksheet.Cells[rowIndex, currentColumnIndex];
+
+                    if (currentCell.Value2?.ToString() == textToSearch)
+                        columnFound = currentColumnIndex;
+                    else
+                        currentColumnIndex++;
+                }
+            }
+
+            return columnFound;
+        }
+
+        /// <summary>
+        /// Search the row where is the text
+        /// </summary>
+        /// <param name="worksheet">Worksheet where realize the searching</param>
+        /// <param name="columnIndex">Index of the column where search the text</param>
+        /// <param name="textToSearch">Text to search</param>
+        /// <returns></returns>
+        public static int GetRowIndex(Worksheet worksheet, int columnIndex, String textToSearch)
+        {
+            int rowFound = -1;
+
+            if (worksheet != null && columnIndex > 0 && !String.IsNullOrWhiteSpace(textToSearch))
+            {
+                int currentRowIndex = 1;
+                while (rowFound == -1 && currentRowIndex < 100)
+                {
+                    Range currentCell = worksheet.Cells[currentRowIndex, columnIndex];
+
+                    if (currentCell.Value2?.ToString() == textToSearch)
+                        rowFound = currentRowIndex;
+                    else
+                        currentRowIndex++;
+                }
+            }
+
+            return rowFound;
+        }
+
+        public static string GetExcelColumnName(int columnIndex)
+        {
+            int dividend = columnIndex;
+            string columnName = String.Empty;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+
+            return columnName;
+        }
+
+        /// <summary>
+        /// Count the number of rows with the selected color
+        /// </summary>
+        /// <param name="worksheet">Worksheet to analyse</param>
+        /// <param name="columnIndex">Index of the column to analyse</param>
+        /// <param name="firstRowIndex">Index of the first row to count</param>
+        /// <param name="lastRowIndex">Index of the last row to count</param>
+        /// <param name="selectedColor">Selected color to search</param>
+        /// <returns>The number of columns with the selected color</returns>
+        public static int CountRowsWithColor(Worksheet worksheet, int columnIndex, int firstRowIndex, int lastRowIndex, Color selectedColor)
+        {
+            int count = 0;
+
+            if (worksheet != null && columnIndex > 0 && firstRowIndex > 0 && lastRowIndex > 0 && selectedColor != null)
+            {
+                int oleColor = ColorTranslator.ToOle(selectedColor);
+
+                for (int i = firstRowIndex; i <= lastRowIndex; i++)
+                {
+                    Range currentCell = worksheet.Cells[i, columnIndex];
+
+                    if ((int)currentCell.Interior.Color == oleColor)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+        #endregion [ Methods ]
+    }
+}
---
+++ ExcelAddIn/Ribbon.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace ExcelAddIn
+{
+    public partial class Ribbon
+    {
+        #region [ Consts ]
+        private const String TAB_NAME = "NEW TAB";
+        #endregion [ Consts ]
+
+
+        #region [ Events ]
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void buttonLaunchTest_Click(object sender, RibbonControlEventArgs e)
+        {
+            Workbook currentDocument = ExcelServices.GetCurrentExcel();
+
+            //Create a new sheet
+            Worksheet newTab = InitializeNewTab(currentDocument);
+
+            if (newTab != null)
+            {
+                List<string> listSheetsName = ExcelServices.GetAllWorksheetsName(currentDocument);
+
+                if (listSheetsName?.Count > 0)
+                {
+                    //Display the name of sheets
+                    for (int i = 0; i < listSheetsName.Count; i++)
+                        newTab.Cells[1, i + 1].Value = listSheetsName[i];
+
+                    //Search a text
+                    String textToSearch = TAB_NAME;
+                    int indexColumn = ExcelServices.GetColumnIndex(newTab, 1, textToSearch);
+                    String letterColumn = ExcelServices.GetExcelColumnName(indexColumn);
+
+                    //Add numbers
+                    for(int i = 1; i<=10; i++)
+                        newTab.Cells[i+1, indexColumn].Value = i;
+
+                    //Add a row Total
+                    int indexRowTotal = 12;
+                    newTab.Cells[indexRowTotal, 1].Value = "TOTAL";
+                    newTab.Cells[indexRowTotal, indexColumn].Value = $"=SUM({letterColumn}2: {letterColumn}11)";
+
+                    //Add colors
+                    StylePage(newTab, indexRowTotal, indexColumn);
+                }
+            }
+        }
+        #endregion [ Events ]
+
+
+        #region [ Functions ]
+        /// <summary>
+        /// Initialize the creation of reviewTab inside the current Excel document
+        /// </summary>
+        /// <param name="excelDoc">Excel document</param>
+        /// <returns>Returns the Worksheet reviewTab initialized</returns>
+        /// <returns></returns>
+        private static Worksheet InitializeNewTab(Workbook excelDoc)
+        {
+            Worksheet newTab = null;
+
+            if (excelDoc != null)
+            {
+                //Check if there is already a sheet review
+                ExcelServices.DeleteWorksheetsWithName(excelDoc, TAB_NAME);
+
+                if (excelDoc?.Worksheets?.Count > 0)
+                {
+                    Worksheet currentLastWorksheet = excelDoc?.Worksheets.Cast<Worksheet>().LastOrDefault();
+
+                    //Add the new sheet at the end
+                    newTab = excelDoc.Worksheets.Add(After: currentLastWorksheet);
+                    newTab.Name = TAB_NAME;
+                }
+            }
+
+            return newTab;
+        }
+
+        private static void StylePage(Worksheet reviewTab, int rowTotalIndex, int columnNumbersIndex)
+        {
+            //Style the page
+            Color COLOR_HEADER_BACKGROUND = Color.FromArgb(75, 172, 198);
+            Color COLOR_HEADER_FOREGROUND = Color.White;
+            Color COLOR_NUMBERS_COLUMN_BACKGROUND = Color.FromArgb(49, 134, 155);
+            Color COLOR_TOTAL_BLOC_BACKGROUND = Color.FromArgb(75, 172, 198);
+
+            String columnNumbersLetter = ExcelServices.GetExcelColumnName(columnNumbersIndex);
+
+            //Column A
+            Range columnA = reviewTab.Range["A:A"];
+            columnA.EntireColumn.ColumnWidth = 20;
+            columnA.Font.Color = Color.Black;
+            columnA.Font.Bold = true;
+            columnA.HorizontalAlignment = XlHAlign.xlHAlignLeft;
+
+            //Column Numbers Header
+            Range rangeColumnNumbersHeader = reviewTab.Range[$"{columnNumbersLetter}1:{columnNumbersLetter}1"];
+            rangeColumnNumbersHeader.Interior.Color = COLOR_HEADER_BACKGROUND;
+            rangeColumnNumbersHeader.Font.Color = COLOR_HEADER_FOREGROUND;
+            rangeColumnNumbersHeader.Font.Bold = true;
+            rangeColumnNumbersHeader.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            rangeColumnNumbersHeader.Borders.LineStyle = XlLineStyle.xlContinuous;
+
+            //Column Numbers
+            Range rangeColumnNumbers = reviewTab.Range[$"{columnNumbersLetter}2:{columnNumbersLetter}{rowTotalIndex - 1}"];
+            rangeColumnNumbers.Interior.Color = COLOR_NUMBERS_COLUMN_BACKGROUND;
+            rangeColumnNumbers.Font.Color = COLOR_HEADER_FOREGROUND;
+            rangeColumnNumbers.Font.Bold = true;
+            rangeColumnNumbers.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            rangeColumnNumbers.Borders.LineStyle = XlLineStyle.xlContinuous;
+            rangeColumnNumbers.Borders.Weight = XlBorderWeight.xlMedium;
+            rangeColumnNumbers.Borders[XlBordersIndex.xlInsideHorizontal].Weight = XlBorderWeight.xlThin;
+
+            //Cell Total
+            Range rangeTotal = reviewTab.Range[$"{columnNumbersLetter}{rowTotalIndex}:{columnNumbersLetter}{rowTotalIndex}"];
+            rangeTotal.Interior.Color = COLOR_HEADER_BACKGROUND;
+            rangeTotal.Font.Color = COLOR_HEADER_FOREGROUND;
+            rangeTotal.Font.Bold = true;
+            rangeTotal.HorizontalAlignment = XlHAlign.xlHAlignCenter;
+            rangeTotal.Borders.LineStyle = XlLineStyle.xlContinuous;
+        }
+        #endregion [ Functions ]
+    }
+}
